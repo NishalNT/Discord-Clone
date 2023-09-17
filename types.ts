64a1a6c03c6b7d1df15a96aad9bc13cf7b5c1391
NishalNT <nishalnthingalaya1@gmail.com>
2023-09-17 21:12:27 +0530

    final

---
+++ types.ts
@@ -0,0 +1,16 @@
+import { Server as NetServer, Socket } from "net";
+import { NextApiResponse } from "next";
+import { Server as SocketIOServer } from "socket.io";
+import { Member, Server,Profile } from "@prisma/client"
+
+export type ServerWIthMembersWithProfiles = Server & {
+    members: (Member & { profile: Profile })[];
+};
+
+export type NextApiResponseServerIo = NextApiResponse & {
+    socket: Socket & {
+        server: NetServer & {
+            io: SocketIOServer;
+        };
+    };
+};
